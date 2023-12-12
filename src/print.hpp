6a09cf05cac65b6319882e435af9ae767400dcf1
David Sugar <tychosoft@gmail.com>
2023-12-12 17:16:15 -0500

    Simplify and use fmt:: in tycho namespace. (#16)
    Reviewed-on: http://luna.home:3000/tychosoft/moderncli/pulls/16

--- src/print.hpp
+++ src/print.hpp
@@ -11,64 +11,30 @@
 #include <unistd.h>
 #endif
 
-#include <fmt/format.h>
+#include <fmt/ranges.h>
 
 namespace tycho {
+using namespace fmt;
+
 template<class... Args>
 auto print_format(std::string_view fmt, const Args&... args) {
     return fmt::format(fmt, args...);
 }
 
 template<class... Args>
-void print(std::string_view fmt, const Args&... args) {
-    auto str = print_format(fmt, args...);
-#if defined(_MSC_VER) || defined(__MINGW32__) || defined(__MINGW64__) || defined(WIN32)
-    fputs(str.data(), stdout);
-    fflush(stdout);
-#else
-    ::write(1, str.data(), str.size());
-#endif
-}
-
-template<class... Args>
-void println(std::string_view fmt = "", const Args&... args) {
-    puts(print_format(fmt, args...).data());
-}
-
-template<class... Args>
-void print(std::FILE *stream, std::string_view fmt, const Args&... args) {
-    fputs(print_format(fmt, args...).c_str(), stream);
-}
-
-template<class... Args>
-void println(const std::FILE *stream, std::string_view fmt = "", const Args&... args) {
-    std::printf(stream, "%s\n", print_format(fmt, args...).c_str()); // FlawFinder: ignore
-}
-
-template<class... Args>
 void die(int code, std::string_view fmt, const Args&... args) {
-    std::cerr << print_format(fmt, args...);
+    std::cerr << fmt::format(fmt, args...);
     ::exit(code);
 }
 
 template<class... Args>
 void print(std::ostream& out, std::string_view fmt, const Args&... args) {
-    out << print_format(fmt, args...);
+    out << fmt::format(fmt, args...);
 }
 
 template<class... Args>
-void println(std::ostream& out, std::string_view fmt = "", const Args&... args) {
-    out << print_format(fmt, args...) << std::endl;
-}
-
-template<class Out, class... Args>
-void print_to(Out& out, std::string_view fmt, const Args&... args) {
-    out << print_format(fmt, args...);
-}
-
-template<class Out, class... Args>
-[[deprecated]] void println_to(Out& out, std::string_view fmt = "", const Args&... args) {
-    out << print_format(fmt, args...) << std::endl;
+void println(std::ostream& out, std::string_view fmt, const Args&... args) {
+    out << fmt::format(fmt, args...) << std::endl;
 }
 } // end namespace
 #endif
