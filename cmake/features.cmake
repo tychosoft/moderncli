80fb0ab43f8448bedfacc2eb20be63a2b7bc3466
David Sugar <tychosoft@gmail.com>
2023-12-13 10:15:49 -0500

    Modernize cmake for fmt (#27)
    Reviewed-on: http://luna.home:3000/tychosoft/moderncli/pulls/27

--- cmake/features.cmake
+++ cmake/features.cmake
@@ -38,10 +38,7 @@ set(CMAKE_THREAD_PREFER_PTHREAD TRUE)
 set(THREADS_PREFER_PTHREAD_FLAG TRUE)
 
 find_package(Threads REQUIRED)
-pkg_check_modules(FMT fmt>=6.1.2 REQUIRED)
-
-include_directories(${FMT_INCLUDE_DIRS})
-link_directories(${FMT_LIBRARY_DIRS})
+find_package(fmt CONFIG REQUIRED)
 
 if(CMAKE_BUILD_TYPE MATCHES "Debug")
     set(BUILD_DEBUG true)
