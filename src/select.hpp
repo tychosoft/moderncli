285989951f016a20660d69a47d1e0a1b982086eb
David Sugar <tychosoft@gmail.com>
2025-01-06 15:25:09 -0500

    Simple counter-map to enum by select

--- src/select.hpp
+++ src/select.hpp
@@ -16,6 +16,8 @@ template <typename... Ts>
 class select_when {
 public:
     using variant_type = std::variant<Ts...>;
+    select_when(const select_when&) = delete;
+    auto operator=(const select_when&) -> auto& = delete;
 
     select_when(std::initializer_list<std::pair<variant_type, std::function<void()>>> list) {
         for (const auto& item : list) {
@@ -40,6 +42,31 @@ public:
 
 private:
     std::unordered_map<variant_type, std::function<void()>> cases_;
+};
+
+template <typename Enum, typename... Ts>
+class select_enum {
+public:
+    using variant_type = std::variant<Ts...>;
+    select_enum(const select_enum&) = delete;
+    auto operator=(const select_enum&) -> auto& = delete;
+
+    select_enum(std::initializer_list<std::pair<variant_type, Enum>> list) {
+        for (const auto& item : list) {
+            cases_[item.first] = item.second;
+        }
+    }
+
+    auto operator()(const variant_type& value) const {
+        auto it = cases_.find(value);
+        if (it != cases_.end())
+            return it->second;
+        throw std::out_of_range("Selection not mapped to an enum");
+    }
+
+private:
+    static_assert(std::is_enum_v<Enum>, "Enum must be an enumerated type");
+    std::unordered_map<variant_type, Enum> cases_;
 };
 } // end namespace
 #endif
--- test/select.cpp
+++ test/select.cpp
@@ -7,13 +7,13 @@
 #include <cstdlib>
 #include <iostream>
 
-static select_when<int, std::string> selector{
-    {1, []() { std::cout << "Case 1\n"; }},
-    {"apple", []() { std::cout << "apple\n"; }},
-    {2, []() { std::cout << "2 Case\n"; }}
-};
-
 auto main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) -> int {
+    const select_when<int, std::string> selector{
+        {1, []() { std::cout << "Case 1\n"; }},
+        {"apple", []() { std::cout << "apple\n"; }},
+        {2, []() { std::cout << "2 Case\n"; }}
+    };
+
     try {
         assert(selector(1));
         assert(selector("apple"));
@@ -22,5 +22,19 @@ auto main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) -> int {
     catch(...) {
         ::exit(-1);
     }
+
+    enum class selects : int {
+        Five = 5,
+        Six = 6,
+        Seven = 7
+    };
+
+    const select_enum<selects, int, std::string> mapped {
+        {5, selects::Five},
+        {"five", selects::Five},
+        {6, selects::Six}
+    };
+
+    assert(mapped("five") == selects::Five);
 }
 
