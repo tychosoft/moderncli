1db9befaaffc443665c76668a8ce3803eabd9f3a
David Sugar <tychosoft@gmail.com>
2025-06-25 11:48:34 -0400

    Improve ranges with container validity check

--- src/ranges.hpp
+++ src/ranges.hpp
@@ -7,8 +7,18 @@
 #include <algorithm>
 #include <iterator>
 #include <numeric>
+#include <type_traits>
 
 namespace tycho::ranges {
+template <typename, typename = std::void_t<>>
+struct is_stl_container : std::false_type {};
+
+template <typename T>
+struct is_stl_container<T, std::void_t<typename T::iterator>> : std::true_type {};
+
+template <typename T>
+constexpr bool is_stl_container_v = is_stl_container<T>::value;
+
 template<typename Predicate>
 class filter {
 public:
@@ -16,7 +26,8 @@ public:
     filter(const filter&) = delete;
     auto operator=(const filter&) -> auto& = delete;
 
-    template <typename Container>
+    template <typename Container,
+    typename = std::enable_if_t<is_stl_container_v<Container>>>
     auto operator()(const Container& container) const {
         Container result{};
         std::copy_if(container.begin(), container.end(), std::back_inserter(result), pred_);
@@ -34,7 +45,8 @@ public:
     transform(const transform&) = delete;
     auto operator=(const transform&) -> auto& = delete;
 
-    template <typename Container>
+    template <typename Container,
+    typename = std::enable_if_t<is_stl_container_v<Container>>>
     auto operator()(const Container& container) const {
         Container result{};
         std::transform(container.begin(), container.end(), std::back_inserter(result), func_);
@@ -45,7 +57,8 @@ private:
     Func func_;
 };
 
-template<typename Container>
+template<typename Container,
+typename = std::enable_if_t<is_stl_container_v<Container>>>
 auto copy(const Container& container, std::size_t pos, std::size_t count) {
     Container result{};
     if(!count || pos >= container.size()) return result;
@@ -55,14 +68,16 @@ auto copy(const Container& container, std::size_t pos, std::size_t count) {
     return result;
 }
 
-template<typename Container>
+template <typename Container,
+typename = std::enable_if_t<is_stl_container_v<Container>>>
 auto take(const Container& container, std::size_t size) {
     Container result{};
     std::copy_n(container.begin(), std::min(size, container.size()), std::back_inserter(result));
     return result;
 }
 
-template<typename Container>
+template<typename Container,
+typename = std::enable_if_t<is_stl_container_v<Container>>>
 auto drop(const Container& container, std::size_t size) {
     Container result{};
     if (size < container.size())
@@ -70,14 +85,16 @@ auto drop(const Container& container, std::size_t size) {
     return result;
 }
 
-template <typename Container>
+template <typename Container,
+typename = std::enable_if_t<is_stl_container_v<Container>>>
 auto join(const Container& a, const Container& b) {
     Container result = a;
     result.insert(result.end(), b.begin(), b.end());
     return result;
 }
 
-template<typename Container, typename Func>
+template<typename Container, typename Func,
+typename = std::enable_if_t<is_stl_container_v<Container>>>
 auto each(Container& container, Func func) {
     auto it = container.begin();
     while(it != container.end()) {
@@ -87,7 +104,8 @@ auto each(Container& container, Func func) {
     return container;
 }
 
-template<typename Container, typename T>
+template<typename Container, typename T,
+typename = std::enable_if_t<is_stl_container_v<Container>>>
 auto contains(const Container& container, T value) {
     auto it = container.begin();
     while(it != container.end()) {
@@ -97,44 +115,52 @@ auto contains(const Container& container, T value) {
     return false;
 }
 
-template<typename Container, typename T, typename Func>
+template<typename Container, typename T, typename Func,
+typename = std::enable_if_t<is_stl_container_v<Container>>>
 auto fold(const Container& container, T init, Func func) -> T {
     return std::accumulate(container.begin(), container.end(), init, func);
 }
 
-template<typename Container>
+template<typename Container,
+typename = std::enable_if_t<is_stl_container_v<Container>>>
 auto count(const Container& container) {
     return std::distance(container.begin(), container.end());
 }
 
-template<typename Container, typename Pred>
+template<typename Container, typename Pred,
+typename = std::enable_if_t<is_stl_container_v<Container>>>
 auto count(const Container& container, Pred&& pred) {
     return std::count_if(container.begin(), container.end(), std::forward<Pred>(pred));
 }
 
-template<typename Container, typename Pred>
+template<typename Container, typename Pred,
+typename = std::enable_if_t<is_stl_container_v<Container>>>
 auto all(const Container& container, Pred&& pred) {
     return std::all_of(container.begin(), container.end(), std::forward<Pred>(pred));
 }
 
-template<typename Container, typename Pred>
+template<typename Container, typename Pred,
+typename = std::enable_if_t<is_stl_container_v<Container>>>
 auto any(const Container& container, Pred&& pred) {
     return std::any_of(container.begin(), container.end(), std::forward<Pred>(pred));
 }
 
-template<typename Container, typename Pred>
+template<typename Container, typename Pred,
+typename = std::enable_if_t<is_stl_container_v<Container>>>
 auto none(const Container& container, Pred&& pred) {
     return std::none_of(container.begin(), container.end(), std::forward<Pred>(pred));
 }
 
-template<typename Container, typename Func>
+template<typename Container, typename Func,
+typename = std::enable_if_t<is_stl_container_v<Container>>>
 auto make(std::size_t size, Func&& func) {
     Container result{};
     std::generate_n(std::back_inserter(result), size, std::forward<Func>(func));
     return result;
 }
 
-template<typename Container, typename Range>
+template<typename Container, typename Range,
+typename = std::enable_if_t<is_stl_container_v<Container>>>
 auto operator|(const Container& container, const Range& range) {
     return range(container);
 }
