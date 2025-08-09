508651f1fad156b2fd4f8c8b39a6f571acda03b8
David Sugar <tychosoft@gmail.com>
2025-08-09 09:09:20 -0400

    Prep for release 1.4.1

--- cmake/linting.cmake
+++ cmake/linting.cmake
@@ -1,5 +1,12 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
-# Copyright (C) 2025 David Sugar <tychosoft@gmail.com>
+# Copyright (C) 2023 Tycho Softworks.
+#
+# This file is free software; as a special exception the author gives
+# unlimited permission to copy and/or distribute it, with or without
+# modifications, as long as this notice is preserved.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 if(LINT_SOURCES)
     find_program(CLANG_TIDY_EXEC "clang-tidy")
